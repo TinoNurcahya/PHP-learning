367c90a1b92e1fe197a63b742dcdffcf7985c759
TinoNurcahya <tinonurcahya@gmail.com>
2025-08-14 22:46:16 +0700

    feat: oop style connect mysql

--- CRUD/connect.php
+++ CRUD/connect.php
@@ -1,12 +1,12 @@
 <?php
-$host = "localhost";  // Sesuaikan dengan host database
-$user = "root";       // Username database
-$pass = "";           // Password database
-$dbname = "sistembarang";    // Nama database
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "sistembarang";
 
-$koneksi = mysqli_connect($host, $user, $pass, $dbname);
+// OOP style
+$koneksi = new mysqli($host, $user, $pass, $dbname);
 
-// Cek koneksi
 if ($koneksi->connect_error) {
   die("Koneksi gagal: " . $koneksi->connect_error);
 }
